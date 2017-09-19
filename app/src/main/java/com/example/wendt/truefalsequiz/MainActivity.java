ec1190737736e4599871799ce24a4c9746d03ba5
skylerwendt <skywendt@gmail.com>
2017-09-19 13:47:56 -0700

    Did some stuff

--- app/src/main/java/com/example/wendt/truefalsequiz/MainActivity.java
+++ app/src/main/java/com/example/wendt/truefalsequiz/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button trueButton, falseButton, nextButton;
     private TextView questionText;
     private List<Question> questionBank;
+    private int questionIndex = 0; //question user is currently on
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +52,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.button_true:
-                //checkAnswer(true);
+                checkAnswer(true);
                 break;
             case R.id.button_false:
-                //checkAnswer(false);
+                checkAnswer(false);
                 break;
             case R.id.button_next:
-                //advanceToNextQuestion();
+                advanceToNextQuestion();
                 break;
             default:
         }
+    }
+
+    /**
+     * Let's compare the users answer with the real answer with help of the Question class's own checkAnswer method
+     * @param userAnswer what the user selected
+     */
+    private void checkAnswer(boolean userAnswer) {
+        if(questionBank.get(questionIndex).checkAnswer(userAnswer)){ //if the user answer is true
+            Toast.makeText(this, "true", Toast.LENGTH_SHORT).show();
+        }
+        else{ //else user answer is false
+            Toast.makeText(this, "false", Toast.LENGTH_SHORT).show();
+        }
+        disableTrueFalseButtons();
+    }
+
+    /**
+     * Sets text to the new question, looping back if all questions have been seen
+     */
+    private void advanceToNextQuestion() {
+        if(questionIndex < questionBank.size()-1){
+            questionIndex++;
+        }
+        else{
+            questionIndex = 0;
+        }
+        questionText.setText(questionBank.get(questionIndex).getQuestionText());
+    }
+
+    private void enableTrueFalseButtons(){
+        trueButton.setEnabled(true);
+        falseButton.setEnabled(true);
+    }
+
+    private void disableTrueFalseButtons() {
+        trueButton.setEnabled(false);
+        falseButton.setEnabled(false);
     }
 }
